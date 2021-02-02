467015d615fe42692fc9f0302da51f4e48a4bd78
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2021-02-01 16:14:14 -0800

    add subscriptions

--- src/customHooks/useDesignManager.js
+++ src/customHooks/useDesignManager.js
@@ -1,14 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import { API, graphqlOperation } from "aws-amplify";
-import { listCustomDesigns } from '../graphql/queries'
+import { listCustomDesigns } from '../graphql/queries';
+import { onCreateCustomDesign, onUpdateCustomDesign } from '../graphql/subscriptions';
 
-export default function useDesignManager (user) {
-    const [designs, setDesigns] = useState([])
+export default function useDesignManager(user) {
+    const [designs, dispatch] = useReducer(reducer, [])
     const [status, setStatus] = useState('loading');
+    const [subs, setSubs] = useState([])
 
     useEffect(() => {
         if (user) {
             fetchDesigns();
+            setUpSubscriptions();
+        }
+        return () => {
+            subs.forEach((sub) => sub.unsubscribe())
         }
     }, [user])
 
@@ -21,14 +27,35 @@ export default function useDesignManager (user) {
                     }
                 }
             }));
-            let items = result.data.listCustomDesigns.items;
-            let sortedItems = items.sort((a,b) => Date.parse(a.createdAt) > Date.parse(b.createdAt) ? 1 : -1)
-            setDesigns(sortedItems);
+            dispatch({ type: 'addDesigns', value: result.data.listCustomDesigns.items});
             setStatus('done');
         } catch (e) {
-            setStatus('failed')
+            setStatus('failed');
             console.log(e);
         }
+    }
+
+    const setUpSubscriptions = () => {
+        //subscribe to new design event
+        let newDesignSub = API.graphql(graphqlOperation(onCreateCustomDesign))
+            .subscribe({
+                next: (newDesignResult) => {
+                    const newDesign = newDesignResult.value.data.onCreateCustomDesign;
+                    dispatch({ type: 'addDesigns', value: [newDesign]});
+                },
+                error: (error) => console.log(error)
+            })
+        //subscribe to design update event
+        let updateDesignSub = API.graphql(graphqlOperation(onUpdateCustomDesign))
+            .subscribe({
+                next: (updatedDesignResult) => {
+                    const updatedDesign = updatedDesignResult.value.data.onUpdateCustomDesign;
+                    //dispatch update action
+                    dispatch({ type: 'updateDesign', value: updatedDesign});
+                },
+                error: (error) => console.log(error)
+            })
+        setSubs([newDesignSub, updateDesignSub])
     }
 
     const refresh = async () => {
@@ -37,4 +64,24 @@ export default function useDesignManager (user) {
     }
 
     return [designs, status, refresh]
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'addDesigns':
+            //enforce uniqueness of new design
+            //i've seen the resolver fire more than one event for the same action
+            //not sure why
+            if (!state.some((design) => design.id === action.value.id)) {
+                return [...state, ...action.value].sort((a, b) => Date.parse(a.createdAt) > Date.parse(b.createdAt) ? 1 : -1)
+            } else {
+                return state;
+            }
+        case 'updateDesign':
+            let newState = state;
+            newState[newState.findIndex((item) => item.id === action.value.id)] = action.value;
+            return newState;
+        default:
+            throw new Error('invalid action type');
+    }
 }
